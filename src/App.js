712bd8874f5b904c16ff065a1d73f22375c99b98
SoniaJayat <SoniaJayat03@gmail.com>
2024-08-21 18:18:09 +0100

    created basic search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello Sonia
+      <div className="container">
+        <div className="title">
+          <header className="App-header"></header>
+          <h1>
+            Dictionary{" "}
+            <img
+              src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/140/380/original/blue_book.png?1724248649"
+              className="dictionary-logo img-fluid"
+            />
+          </h1>
+        </div>
+
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            This project was coded by Sonia Jayatheesan and is open-sourced on
+            GitHub
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  const [keyword, setKeyword] = useState(null);
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword} definition`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+        <input
+          type="submit"
+          value="Search"
+          className="submit btn btn-primary"
+        />
+      </form>
+    </div>
+  );
+}
